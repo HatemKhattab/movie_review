9eeb154057319d0bb2c4333d15702aaba47435a0
HatemKhattab <hattemkhattab@gmail.com>
2015-12-30 20:57:43 +0000

    Fix upload photos

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151230095636) do
+ActiveRecord::Schema.define(version: 20151230183110) do
 
   create_table "movies", force: :cascade do |t|
     t.text     "description"
     t.string   "movie_lenght"
     t.string   "director"
     t.string   "rating"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.integer  "user_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+    t.string   "title"
   end
 
   create_table "users", force: :cascade do |t|
