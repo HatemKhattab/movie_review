8dbee630c55796de7a710572812fffdd20052476
HatemKhattab <hattemkhattab@gmail.com>
2015-12-31 09:25:54 +0000

    add reviews

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151230183110) do
+ActiveRecord::Schema.define(version: 20151231085456) do
 
   create_table "movies", force: :cascade do |t|
     t.text     "description"
@@ -28,6 +28,14 @@ ActiveRecord::Schema.define(version: 20151230183110) do
     t.string   "title"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "rating"
+    t.text     "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -41,6 +49,7 @@ ActiveRecord::Schema.define(version: 20151230183110) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
