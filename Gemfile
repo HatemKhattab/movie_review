2882b549fb81ca0e4b041223041ab048f885c787
HatemKhattab <hattemkhattab@gmail.com>
2016-01-03 09:46:25 +0000

    change dabase to postqresqlo

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rails', '4.2.2'
-gem 'sqlite3', '1.3.9'
+gem 'pg'
 gem 'web-console', '2.0.0.beta3'
 gem 'sass-rails', '~> 4.0.3'
 gem 'uglifier', '>= 1.3.0'
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151231092822) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "movies", force: :cascade do |t|
     t.text     "description"
     t.string   "movie_lenght"
@@ -53,7 +56,7 @@ ActiveRecord::Schema.define(version: 20151231092822) do
     t.integer  "user_id"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
